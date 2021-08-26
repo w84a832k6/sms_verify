49db1272aca775520c1773e0aea75931a8a1d949
w84a832k6 <w84a832k6@gmail.com>
2021-08-27 00:25:14 +0800

    first commit

---
+++ app/src/main/java/com/example/smsverify/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.smsverify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.scanQrButton -> {
+                val intent = Intent(this, ScanQrcode::class.java)
+                this.startActivity(intent)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
